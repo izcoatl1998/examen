4d1e05d799178b14e58e8f793ae8a94728498aa3
izcoatl1998 <48344749+izcoatl1998@users.noreply.github.com>
2019-05-02 14:03:16 -0500

    Usado un Do while

--- examen.c
+++ examen.c
@@ -6,7 +6,8 @@ void TIMER0(){
 set_timer0(15536);
 }
 
-int i=0,calculo=0;
+int i=0,calculo=0,calculo2=0,separador=0,puntero=-1;
+int c2=0,acabar=0;
 void main()
 {
    setup_timer_0(RTCC_INTERNAL | RTCC_DIV_8 );
@@ -17,19 +18,41 @@ void main()
    {
       if(kbhit()){
          caracter[i]=getch();
-      if(caracter[i]>=65 && caracter[i]<=90 || caracter[i]>=97&&caracter[i]<=122|| caracter[i]==32){
+      if(caracter[i]>=65 && caracter[i]<=90 || caracter[i]==32){
             printf("%c",caracter[i]);
        if(caracter[i]==32){
             calculo=1;
              }
+           i++;
         }
        }
-   if(calculo==2){
+    do{
+      if(calculo==1){
+         i++;
+         separador=i;
       if(kbhit()){
          caracter[i]=getch();
-      if(caracter[i]>=65 && caracter[i]<=90 || caracter[i]>=97&&caracter[i]<=122|| caracter[i]==13){
+      if(caracter[i]>=65 && caracter[i]<=90 || caracter[i]==13){
             printf("%c",caracter[i]);}
+            if(caracter[i]==13){
+            calculo2=1;}
+            
       }
-   }
+      if(calculo2==1){
+      switch(caracter[separador]){
+          case 't':
+             do{
+             puntero++;
+             printf("%c",caracter[puntero]);
+             if(caracter[puntero]==32){
+             puntero=-1;
+             c2++;}
+             }while(c2==3);
+             acabar=1;
+            break;
+          }
+       }
+      }
+     }while(acabar=!1);
    }
 }
